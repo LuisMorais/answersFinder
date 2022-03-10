167474199cbb7996fcd8b215f1f97242a8ed039a
GabrielN11 <gabrielnunes11c@gmail.com>
2022-03-10 16:26:32 -0300

    fix

--- script/auth/validateFields.js
+++ script/auth/validateFields.js
@@ -1,35 +1,22 @@
-export const getLoginFields = () => {
-    const email = document.querySelector('#username')
-    const password = document.querySelector('#password')
-    return [email, password]
-}
-
-export const getSignupFields = () => {
-    const email = document.querySelector('#email')
-    const password = document.querySelector('#password')
-    const confirmPassword = document.querySelector('#confirm-password')
-    const username = document.querySelector('#username')
-    return [email, password, confirmPassword, username]
-}
-
 window.addEventListener('load', () => {
     const isEmpty = [true, true, true, true]
+    let isEqual = true
     const button = document.querySelector('#button')
-    button.disabled = true
 
     const fields = location.pathname.includes('login') ? getLoginFields() : getSignupFields()
 
 
     fields.forEach((element, i) => {
-        element.value = ''
         element.addEventListener('input', (event) => {
             if (event.target.value.length > 0) isEmpty[i] = false
             else isEmpty[i] = true
             const isArrayTrue = verifyIsEmpty(isEmpty, fields)
             if (isArrayTrue) {
                 button.disabled = true
-            }else{
+            } else if(isEqual) {
                 button.disabled = false
+            }else{
+                button.disabled = true
             }
         })
     })
@@ -41,4 +28,18 @@ function verifyIsEmpty(isEmpty, fields) {
         if (!isEmpty[i]) return false
         return true
     }, false)
+}
+
+function getLoginFields() {
+    const email = document.querySelector('#username')
+    const password = document.querySelector('#password')
+    return [email, password]
+}
+
+function getSignupFields() {
+    const email = document.querySelector('#email')
+    const password = document.querySelector('#password')
+    const confirmPassword = document.querySelector('#confirm-password')
+    const username = document.querySelector('#username')
+    return [email, password, confirmPassword, username]
 }
