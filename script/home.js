ec6335293d7fd10e1196a37b4434ee7526fca5a4
GabrielN11 <gabrielnunes11c@gmail.com>
2022-03-11 14:45:04 -0300

    login e logout

--- script/home.js
+++ script/home.js
@@ -1,10 +1,87 @@
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js";
+import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js";
+import { ref, getDatabase, onValue } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-database.js"
 
 document.body.onload = () => {
-    const user = getAuth()
-    console.log(user)
-    if(user.currentUser){
-        document.querySelector('#auth-link').remove()
+    const auth = getAuth()
+    const db = getDatabase();
+
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            removeAuthLinks()
+            const uid = user.uid
+            return onValue(ref(db, '/users/' + uid), (snapshot) => {
+                const username = (snapshot.val() && snapshot.val().userName) || 'Anonymous'
+                setProfileLink(username)
+            }, {
+                onlyOnce: false
+            })
+        } else {
+            console.log('blabla')
+        }
+
+    })
+}
+
+function removeAuthLinks() {
+    const authLink = document.querySelector('#auth-link')
+    authLink.remove()
+}
+
+function setProfileLink(username) {
+    const profileDiv = document.createElement('div')
+    profileDiv.classList.add('username')
+    const icon = document.createElement('i')
+    icon.classList.add('fa', 'fa-user')
+    icon.setAttribute('aria-hidden', true)
+    const name = document.createElement('span')
+    name.innerText = username
+
+    const userMenu = document.createElement('nav')
+    userMenu.classList.add('usermenu')
+
+    const profileLink = document.createElement('a')
+    profileLink.setAttribute('href', '#') //mudar depois
+    profileLink.innerText = 'Perfil'
+
+    const logout = document.createElement('div')
+    logout.innerText = 'Sair'
+    setLogout(logout)
+
+    userMenu.append(profileLink, logout)
+
+    profileDiv.append(icon, name, userMenu)
+
+    setDropdownEvents(profileDiv, userMenu)
+
+    document.querySelector('#header').append(profileDiv)
+}
+
+function setLogout(elem) {
+    const auth = getAuth();
+    
+    elem.addEventListener('click', () => {
+        signOut(auth).then(() => {
+            location.href = 'http://127.0.0.1:5500'
+        }).catch((error) => {
+            // An error happened.
+        });
+    })
+}
+
+function setDropdownEvents(parent, child) {
+    const show = () => {
+        child.style.display = 'block'
     }
 
+    const hide = () => {
+        child.style.display = 'none'
+    }
+
+    const toggle = () => {
+        child.style.display = child.style.display === 'block' ? 'none' : 'block'
+    }
+
+    parent.addEventListener('mouseover', show)
+    parent.addEventListener('click', toggle)
+    parent.addEventListener('mouseleave', hide)
 }
